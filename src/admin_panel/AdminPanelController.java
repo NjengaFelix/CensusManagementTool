c2cd9585fe83235a40b78ad5bcf5c1eacdeddf28
Felix Njenga <32095327+njengafelix@users.noreply.github.com>
2019-12-01 11:04:37 +0300

    Admin upload a category feature added. IntelliJ notNull annotations added

--- src/admin_panel/AdminPanelController.java
+++ src/admin_panel/AdminPanelController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,13 +21,16 @@ public class AdminPanelController {
     @FXML
     private Button mUploadCatBtn, mManageSupBtn, mOpenTextEditorBtn;
     @FXML
-    private Label mCatFileLbl, mLeftStatusLbl, mRightStatusLbl;
+    private Label mLeftStatusLbl, mRightStatusLbl;
     @FXML
     private ScrollPane uploadCatSP;
     @FXML
     private AnchorPane mUploadAP;
     private Stage stage = new Stage();
     private FileChooser fileChooser = new FileChooser();
+    //Creating an array to save already uploaded file names. Maximum number of categories is 5
+    private String[] fileNames = new String[5];
+    private int i, y = 0;
 
     @FXML
     private void onClickNavigateBtn(ActionEvent actionEvent) {
@@ -43,7 +47,12 @@ public class AdminPanelController {
             mLeftStatusLbl.setText("Upload category view");
             uploadCatSP.setVisible(true);
             configureFileChooser(fileChooser);
-            listUploadedFiles(fileChooser.showOpenDialog(stage));
+            //Can only update 5 categories
+            if(i<5) {
+                listUploadedFiles(fileChooser.showOpenDialog(stage));
+            } else {
+                mLeftStatusLbl.setText("Can only update five categories");
+            }
         } else if(actionEvent.getSource() == mManageSupBtn) {
             mLeftStatusLbl.setText("Manage supervisor view");
         } else {
@@ -61,15 +70,38 @@ public class AdminPanelController {
         fileChooser.getExtensionFilters().add(extensionFilter);
     }
 
-    private void listUploadedFiles(File file) {
-        //TODO: listing of the uploaded files well
+    private void listUploadedFiles(@NotNull File file) {
         String fileName = file.getName();
-        mCatFileLbl.setText(fileName);
-        Label label = new Label(fileName);
-        AnchorPane anchorPane = new AnchorPane();
-        anchorPane.setMaxHeight(150);
-        anchorPane.setMaxWidth(350);
-        mUploadAP.getChildren().add(anchorPane);
-        anchorPane.getChildren().add(label);
+
+        try{
+            for(String name : fileNames) {
+                if (name.contains(fileName)) {
+                    mLeftStatusLbl.setText("File already uploaded");
+                    break;
+                } else {
+                    configureFileView(fileName);
+                    fileNames[i] = fileName;
+                    //Change the next position for the name to be saved
+                    i++;
+                    break;
+                }
+            }
+        } catch (NullPointerException e) {
+            configureFileView(fileName);
+            fileNames[i] = fileName;
+            //Change the next position for the name to be saved
+            i++;
+        }
+    }
+
+    private void configureFileView(String labelName) {
+        AnchorPane filePane = new AnchorPane();
+        filePane.setMaxSize(425, 50);
+        mUploadAP.getChildren().add(filePane);
+        filePane.setLayoutY(y);
+        Label label1 = new Label(labelName);
+        filePane.getChildren().add(label1);
+        //Change the y layout position
+        y+=50;
     }
 }
